87b5152bc2ce8154174b52b03a5c8fc83459bdd5
kkomakk <j2031414@gmail.com>
2026-01-22 14:33:47 +0900

    웹 페이지 최종본

--- projectEx/src/main/webapp/js/index.js
+++ projectEx/src/main/webapp/js/index.js
@@ -1,3 +1,42 @@
 /**
  * 
  */
+
+$(function(){
+	//scroll 이벤트
+	$(window).on('scroll',function(){
+		//스크롤 되는 문서의 스크롤 탑이 헤더박스의 높이 이상이면 
+		//메인메뉴 고정 그림자 표시
+		if($(document).scrollTop() >= $('headerBox').height())
+			$('#mainMenuBox').addClass('mainMenuFixed mainMenuShadow');
+		else
+			$('#mainMenuBox').removeClass('mainMenuFixed mainMenuShadow');
+		
+	});//scroll on 끝
+	//moveToTop 이미지 클릭 시 top 이동
+	$('#moveToTop').on('click',function(){
+		$('html,body').animate({scrolllTop:0}, 500);
+	});
+});//ready 끝
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
